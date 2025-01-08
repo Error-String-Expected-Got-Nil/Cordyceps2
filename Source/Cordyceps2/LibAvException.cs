db6bb5187e4cac6a9c3d828687fcb09e8af6813d
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-01-07 18:56:38 -0600

    LibAvException
    Exception type for automatically appending libav error codes. Unused right now, but may be added if I want more robust logging later.

---
+++ Source/Cordyceps2/LibAvException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using FFmpeg.AutoGen;
+
+namespace Cordyceps2;
+
+public class LibAvException(string message, int libavErrorCode)
+    : Exception(message + " (libav error: " + TranslateLibAvError(libavErrorCode) + ")")
+{
+    public static unsafe string TranslateLibAvError(int errorCode)
+    {
+        const int bufsize = 1024;
+        var buffer = stackalloc byte[bufsize];
+        ffmpeg.av_strerror(errorCode, buffer, bufsize);
+        return Marshal.PtrToStringAnsi((IntPtr)buffer);
+    }
+}
