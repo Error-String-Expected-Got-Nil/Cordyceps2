8873a04563a3f7807bcc1e90833c7993b30995d0
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-01-07 18:55:43 -0600

    Main encoder thread function

--- Source/Cordyceps2/Encoder.cs
+++ Source/Cordyceps2/Encoder.cs
@@ -6,7 +6,16 @@ using Object = UnityEngine.Object;
 
 namespace Cordyceps2;
 
-// This class is blanket unsafe since it's dealing with unmanaged libav data everywhere
+// This class is blanket unsafe since it's dealing with unmanaged libav data everywhere.
+//
+// Usage: Constructor performs initial setup and configuration of codecs and muxer. Start() begins the codec and muxer
+// threads, also taking the path to the directory you want videos output to. Returns a Task<bool> which completes when
+// all threads have been started and the Encoder is ready to accept input data, or if something went wrong. Value will
+// be true if Encoder started successfull, false or faulted if it didn't.
+//
+// If an Encoder is stopped, or its return for Start() is false or faulted, it cannot be restarted. You must dispose it
+// and instantiate a new Encoder to try again.
+
 public unsafe class Encoder : IDisposable
 {
     public const AVCodecID VideoCodec = AVCodecID.AV_CODEC_ID_H264; 
@@ -15,23 +24,33 @@ public unsafe class Encoder : IDisposable
     private readonly ConcurrentQueue<Pointer<AVPacket>> _packetQueue = new();
 
     private readonly BufferedEventWaitHandle _videoDataSubmitted = new();
+    private readonly BufferedEventWaitHandle _packetSubmitted = new();
     
     private SwsContext* _frameFormatter;
+    private int _inputLinesize;
     private AVCodec* _videoCodec;
     private AVCodecContext* _videoCodecContext;
+
+    private long _framecount;
     
+    private bool _forcedStop;
     private bool _disposed;
     
     public bool Running { get; private set; }
     public bool Stopping { get; private set; }
+
+    public readonly VideoSettings VideoConfig;
     
     public Encoder(VideoSettings conf)
     {
+        VideoConfig = conf;
+        
         _frameFormatter = ffmpeg.sws_getContext(
             conf.VideoInputWidth, conf.VideoInputHeight, AVPixelFormat.AV_PIX_FMT_RGBA,
             conf.VideoOutputWidth, conf.VideoOutputHeight, AVPixelFormat.AV_PIX_FMT_YUV420P,
             ffmpeg.SWS_BILINEAR, null, null, null
         );
+        _inputLinesize = ffmpeg.av_image_get_linesize(AVPixelFormat.AV_PIX_FMT_RGBA, conf.VideoInputWidth, 0);
         
         _videoCodec = ffmpeg.avcodec_find_encoder(VideoCodec);
         if (_videoCodec == null) throw new EncoderException("Could not find codec with ID: " + VideoCodec);
@@ -46,6 +65,9 @@ public unsafe class Encoder : IDisposable
         _videoCodecContext->bit_rate = 0; // Bitrate is determined by CRF
         ffmpeg.av_opt_set_double(_videoCodecContext->priv_data, "crf", conf.ConstantRateFactor, 0);
         ffmpeg.av_opt_set(_videoCodecContext->priv_data, "preset", conf.Preset, 0);
+        
+        if (ffmpeg.avcodec_open2(_videoCodecContext, _videoCodec, null) < 0)
+            throw new EncoderException("Failed to open video codec.");
     }
 
     public void SubmitVideoData(Texture2D data)
@@ -58,14 +80,119 @@ public unsafe class Encoder : IDisposable
     
     private void VideoCodecThread()
     {
+        Texture2D current = null;
+        AVPacket* packet = null;
+        
+        var inputData = new byte*[1];
+        var linesize = new[] { _inputLinesize };
+
+        // Only need to allocate frame once. The buffers created by av_frame_get_buffer are ref-counted, and the codec
+        // will create its own reference to those buffers to save the data it needs. Calling av_frame_make_writable
+        // will check if there is more than one reference, and create new buffers for the frame if there are. Thus, the
+        // only thing that changes inside this frame are those buffers, so we only need to allocate one instance of the
+        // frame, and set constant fields (width, height, pixel format) once.
+        var frame = ffmpeg.av_frame_alloc();
+        if (frame == null) throw new EncoderException("Failed to allocate video codec AVFrame.");
+
+        frame->width = VideoConfig.VideoOutputWidth;
+        frame->height = VideoConfig.VideoOutputHeight;
+        frame->format = (int)AVPixelFormat.AV_PIX_FMT_YUV420P;
+                
+        if (ffmpeg.av_frame_get_buffer(frame, 0) < 0)
+            throw new EncoderException("Failed to allocate buffers for video codec AVFrame.");
+        
         try
         {
-            
+            while (true)
+            {
+                _videoDataSubmitted.WaitOne();
+
+                if (_forcedStop) break;
+
+                if (!_videoDataQueue.TryDequeue(out current))
+                {
+                    if (Stopping) break;
+                    continue;
+                }
+
+                if (ffmpeg.av_frame_make_writable(frame) < 0)
+                    throw new EncoderException("Failed to make video codec AVFrame writable.");
+
+                var buffer = current.GetRawTextureData();
+                Object.Destroy(current);
+                current = null;
+
+                fixed (byte* pixelData = buffer)
+                {
+                    // sws_scale is designed to work with multi-plane pixel formats, and as such accepts the input data
+                    // and linesize arguments as *arrays*, where each element is the data/linesize for the respective
+                    // plane in the input format. In this case the arrays have been pre-allocated at the start of this
+                    // method, and we just need to set the pointer for the inputData, since the linesize is constant.
+                    inputData[0] = pixelData;
+                    
+                    ffmpeg.sws_scale(
+                        _frameFormatter,
+                        inputData, linesize,
+                        0, VideoConfig.VideoInputHeight,
+                        frame->data, frame->linesize
+                    );
+                }
+
+                frame->pts = _framecount;
+                _framecount++;
+
+                if (ffmpeg.avcodec_send_frame(_videoCodecContext, frame) < 0)
+                    throw new EncoderException("Error on attempting to encode video frame.");
+
+                // As per FFmpeg/libav documentation, it is technically permissible for an encoder to produce more than
+                // one packet per sent frame. I don't think the H264 codec does this, but for the sake of robustness
+                // I'll be implementing it as if it might.
+                while (true)
+                {
+                    if (packet == null) packet = ffmpeg.av_packet_alloc();
+                    if (packet == null) throw new EncoderException("Failed to allocate video codec output packet.");
+
+                    var ret = ffmpeg.avcodec_receive_packet(_videoCodecContext, packet);
+                    
+                    // Packet successfully received
+                    if (ret == 0)
+                    {
+                        SubmitPacket(packet);
+                        packet = null;
+                        continue;
+                    }
+                    
+                    // No packet retrieved because encoder needs more input data, repeat outer loop to get next frame.
+                    if (ret == ffmpeg.EAGAIN) break;
+                    
+                    // Otherwise, some other error occurred.
+                    throw new EncoderException("Error on attempting to retrieve encoded video packet.");
+                }
+            }
         }
         finally
         {
-            
+            if (current is not null) Object.Destroy(current);
+            ffmpeg.av_frame_free(&frame);
+            ffmpeg.av_packet_free(&packet);
         }
+    }
+
+    private void SubmitPacket(AVPacket* packet)
+    {
+        if (_forcedStop) return;
+        
+        _packetQueue.Enqueue(packet);
+        _packetSubmitted.Set();
+    }
+
+    // TODO: Implement
+    // Procedure something like: Set Stopping to true to stop taking input, signal codec and muxer threads one more
+    // time so they clear their queue and see that they're finished.
+    // Once codecs and muxer threads return, flush the codecs, finish writing file, etc.
+    public void Stop()
+    {
+        throw new NotImplementedException();
     }
     
     // Synchronous hard stop, makes all codec and muxer threads stop at the first opportunity, WITHOUT ensuring the
