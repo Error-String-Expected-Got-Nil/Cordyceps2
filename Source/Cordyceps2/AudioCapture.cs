ec7b5bd0345859fd889fc18edcc678e16eead01d
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-02-04 12:44:53 -0600

    Audio capture start

---
+++ Source/Cordyceps2/AudioCapture.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using UnityEngine;
+
+namespace Cordyceps2;
+
+// Must be attached to the game's AudioListener
+public class AudioCapture : MonoBehaviour
+{
+    private int _requestedSamples;
+
+    public void RequestSamples(int count) => Interlocked.Add(ref _requestedSamples, count);
+    
+    
+}
